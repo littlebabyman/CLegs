littlebabyman/CLegs

commit 6fa3b4711e5e69bd8ab4c2b33cbee708df7906f1

Remove unneeded PLAYER:Alive check in ENT:Think
We already do this in our autorun Think hook.

lua/entities/firstperson_legs.lua
@@ -58,11 +58,9 @@ local eGetPoseParameter, eSetPoseParameter = ENTITY.GetPoseParameter, ENTITY.Set
 local eInvalidateBoneCache = ENTITY.InvalidateBoneCache
 local eGetNumBodyGroups = ENTITY.GetNumBodyGroups
 local eGetBodygroup, eSetBodygroup = ENTITY.GetBodygroup, ENTITY.SetBodygroup
-local pAlive = PLAYER.Alive
 local eSetNextClientThink = ENTITY.SetNextClientThink
 local aCurTime = CurTime
 local haveLayeredSequencesBeenFixed = false
-local wasAlive = false
 local lastBodygroupApply = 0
 local lastModelScale = nil
 
@@ -114,14 +112,6 @@ function ENT:Think()
 
         lastBodygroupApply = curTime
     end
-
-    local isAlive = pAlive(ply)
-
-    if !wasAlive and isAlive then
-        self:DoBoneManipulation()
-    end
-
-    wasAlive = isAlive
 
     -- Set the next think to run as soon as possible, i.e. the next frame.
     eSetNextClientThink(self, curTime)

commit a8948fa442417589c22c2fe2f009569035bd8aa9

Fix missing PLAYER:Alive function
caused by 6fa3b47

lua/entities/firstperson_legs.lua
@@ -166,6 +166,7 @@ local function ExternalShouldDraw(plyTable)
     return VWallrunning or inmantle or (pk_pills and pk_pills.getMappedEnt(ply)) or (plyTable.IsProne and plyTable.IsProne(ply)) or (VManip and VMLegs:IsActive())
 end
 
+local pAlive = PLAYER.Alive
 local pShouldDrawLocalPlayer = PLAYER.ShouldDrawLocalPlayer
 local pGetObserverTarget = PLAYER.GetObserverTarget
 local aGetViewEntity = GetViewEntity